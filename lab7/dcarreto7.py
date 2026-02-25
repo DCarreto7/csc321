f3fed86b652418270fde8d8b75295bc4d8062f36
DCarreto7 <dominiccarreto@gmail.com>
2026-02-25 01:49:42 +0000

    adds Lab 7 python code

---
+++ lab7/dcarreto7.py
@@ -0,0 +1,19 @@
+#Dominic Carreto
+#Lab 7
+
+IntVal = 28
+FloatVal = 3.14
+StrVal = "Code"
+
+if IntVal < 20:
+    print("IntVal ", IntVal," is less than 20.", IntVal)
+elif IntVal > 20:
+    print("IntVal ", IntVal," is greater than 20.")
+if FloatVal > 4:
+    print("FloatVal ", FloatVal, " is greater than 4")
+elif FloatVal < 4:
+    print("FloatVal ", FloatVal, " is less than 4")
+if StrVal == "Code":
+    print("StrVal is equal too Code")
+elif StrVal != "Code":
+    print("StrVal is not equal too Code")
