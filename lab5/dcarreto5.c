6b6ea339d636b02bcfedac1aef48cf6f3e79cf62
DCarreto7 <dominiccarreto@gmail.com>
2026-03-05 16:34:48 -0800

    Fixes Indentation for Lab 5 .c file

--- lab5/dcarreto5.c
+++ lab5/dcarreto5.c
@@ -9,28 +9,26 @@
 int main(void)
 {
 	int w = 25;
-    	char x = 'E';
-    	float y = 6.43;
-    	double z = 9.34;
+    char x = 'E';
+    float y = 6.43;
+    double z = 9.34;
 
-    	printf("%d, %p\n", w, &w);
-    	printf("%c, %p\n", x, &x);
-    	printf("%f, %p\n", y, &y);
-    	printf("%f, %p\n\n", z, &z);
+    printf("%d, %p\n", w, &w);
+    printf("%c, %p\n", x, &x);
+    printf("%f, %p\n", y, &y);
+    printf("%f, %p\n\n", z, &z);
 	
-        if(w != 25)
-        {
-        	int e = 1;
-        }
-//      printf("printing w = %d \n", e);
-
+    if(w != 25)
+    {
+        int e = 1;
+	}
+    //printf("printing w = %d \n", e);
 	int array[10] = {5,10,15,20,25,30,35,40,45,50};
 	int i=0;
 	for(i=0; i<10; i++)
 	{
-        	printf("array[%d] = %d \n",i, array[i]);
+        printf("array[%d] = %d \n",i, array[i]);
 		printf("array[%d] address = %p \n",i, &array[i]);
-    	}
-
+    }
 	return EXIT_SUCCESS;
 }
